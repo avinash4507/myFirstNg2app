0b7f580f2294eec28c1ae9f1c1a5821977029339
avinash <avinash4507@gmail.com>
2016-04-17 04:52:09 +0530

    navbar active class

--- app/navbar/navbar.component.ts
+++ app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from 'angular2/core';
-import { ROUTER_DIRECTIVES } from 'angular2/router';
+import { Router, ROUTER_DIRECTIVES } from 'angular2/router';
 
 @Component({
     selector: 'navbar',
@@ -8,5 +8,12 @@ import { ROUTER_DIRECTIVES } from 'angular2/router';
 })
 
 export class NavbarComponent {
+    constructor(private _router: Router){
+        
+    }
     
+    isCurrentRoute(route) {
+        var instruction = this._router.generate(route);
+        return this._router.isRouteActive(instruction);
+    }    
 }
